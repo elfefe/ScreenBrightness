64ecfe7876fa18cd4b6ec5e4d16df9d0d86a732f
elfefe <f.bou-reiff@orange.fr>
2024-11-22 18:53:05 +0100

    feat:  - Init page

--- app/src/main/java/com/elfefe/screenbrightness/Config.kt
+++
@@ -1,8 +0,0 @@
-package com.elfefe.screenbrightness
-
-import androidx.compose.runtime.Composable
-
-@Composable
-fun Configs() {
-    
-}
--- app/src/main/java/com/elfefe/screenbrightness/Schedule.kt
+++
@@ -1,8 +0,0 @@
-package com.elfefe.screenbrightness
-
-data class Schedule(
-    val hour: Int,
-    val minute: Int,
-    val daysOfWeek: Set<Int>,
-    val isScheduled: Boolean
-)
